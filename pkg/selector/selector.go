1f4a2d3cf8b735a43d15a7c66f44233e730ba029
Yuta Nakamura <nakamurayuta0204@gmail.com>
2025-02-16 23:34:15 +0900

    feat: ci

--- pkg/selector/selector.go
+++ pkg/selector/selector.go
@@ -5,10 +5,12 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
+// SelectorItem represents an item that can be selected through the interactive selector
 type SelectorItem interface {
 	Label() string
 }
 
+// SelectItem presents a list of items to the user and returns the selected item
 func SelectItem[T SelectorItem](items []T, prompt string) (T, error) {
 	labels := make([]string, len(items))
 	for i, item := range items {
@@ -28,6 +30,7 @@ func SelectItem[T SelectorItem](items []T, prompt string) (T, error) {
 	return items[i], nil
 }
 
+// SelectContainerDefinition presents a list of container definitions to the user and returns the selected one
 func SelectContainerDefinition(containerDefinitions []types.ContainerDefinition, prompt string) (types.ContainerDefinition, error) {
 	labels := make([]string, len(containerDefinitions))
 	for i, item := range containerDefinitions {

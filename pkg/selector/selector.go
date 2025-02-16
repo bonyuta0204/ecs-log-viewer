0894597ef947768617340d76612c7fa45ecd631e
Yuta Nakamura <nakamurayuta0204@gmail.com>
2025-02-16 23:37:24 +0900

    feat: ci

--- pkg/selector/selector.go
+++ pkg/selector/selector.go
@@ -5,13 +5,13 @@ import (
 	"github.com/manifoldco/promptui"
 )
 
-// SelectorItem represents an item that can be selected through the interactive selector
-type SelectorItem interface {
+// selectorItem represents an item that can be selected through the interactive selector
+type selectorItem interface {
 	Label() string
 }
 
 // SelectItem presents a list of items to the user and returns the selected item
-func SelectItem[T SelectorItem](items []T, prompt string) (T, error) {
+func SelectItem[T selectorItem](items []T, prompt string) (T, error) {
 	labels := make([]string, len(items))
 	for i, item := range items {
 		labels[i] = item.Label()
